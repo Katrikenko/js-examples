d2d2027015ad8b1e61b016b3f872fc03dae7cb0c
Kateryna Lysenko <katrikenko@gmail.com>
2023-07-24 14:14:34 +0200

    end regexp lesson

--- index.js
+++ index.js
@@ -1,91 +1,43 @@
-/* const counterLabel = "Timer";
+const regexp = /(?<group1>.e)(?<group2>st)/gims;
 
-console.time(counterLabel);
-console.count(counterLabel);
+// const regexp1 = RegExp("еest", "g");
+
+// console.log(regexp.flags);
+// console.log(regexp.global);
+// console.log(regexp.multiline);
+// console.log(regexp.dotAll);
+// console.log(regexp.ignoreCase);
+
+// const result = regexp.exec("This is test and est");
+
+// // console.log(result[2]);
+// // console.log(result);
+// console.log(result.groups);
+
+// console.log(regexp.exec("This is test and est"));
+// console.log(regexp.exec("This is test and est"));
+
+// console.log(regexp.test("This is test and est"));
+
+// console.log(regexp.source);
+
+// //======
+
+// regexp.lastIndex = 9;
+
+// console.log(regexp.exec("This is test and est"));
+// console.log(regexp.exec("This is test and est"));
 
 //=====
 
-{
-  let a = 5;
+const test = "This is test and est";
 
-  a = 10;
+console.log(test.replace(regexp, "###"));
 
-  console.error("Error: variable 'a' must be 5");
-  //   console.expection("Error");
+console.log(test.search(regexp));
+// console.log(test.match(regexp));
 
-  console.info("Information");
+const result1 = test.matchAll(regexp);
 
-  console.warn("Warning! Potential problems");
-
-  console.debug("For testing and fixing bugs");
-}
-//=========
-console.timeLog(counterLabel);
-
-console.count(counterLabel);
-//=========
-console.group("Group 1");
-
-console.log("Test");
-console.debug("Test 2");
-
-console.groupCollapsed("Group 2");
-
-console.warn("Test 3");
-
-console.groupEnd();
-
-console.groupEnd();
-
-//========
-
-console.timeEnd(counterLabel);
-
-console.countReset(counterLabel);
-
-console.count(counterLabel);
-
-*/
-
-//=======================================================================
-
-// const test1 = () => console.trace("Hello!");
-
-// const test2 = () => test1();
-// const test3 = () => test2();
-
-// test3();
-//=======================================================================
-
-// const data = [
-//   { name: "John", age: 25, city: "New York" },
-//   { name: "Alice", age: 30, city: "London" },
-// ];
-
-// // console.table(data);
-// console.table(data, ["name", "age"]);
-
-//===========================================================
-
-const a = 5;
-const b = 10;
-
-const result = a > b;
-
-console.assert(result, "Info");
-// if (a > b) {
-//   //....
-// } else {
-//   console.warn("Info");
-// }
-
-//====
-
-// console.clear(); // clear terminal
-
-//===========
-
-console.log(
-  "%cHello World",
-  "color: yellow; background-color: blue; font-size: 50px"
-);
+console.log(result1.next());
+console.log(result1.next());
--- index1.js
+++ index1.js
@@ -2300,3 +2300,99 @@ console.log(Math.fround(0.1 + 0.2) === Math.fround(0.3));
 */
 
 //====================================================
+
+/* const counterLabel = "Timer";
+
+console.time(counterLabel);
+console.count(counterLabel);
+
+//=====
+
+{
+  let a = 5;
+
+  a = 10;
+
+  console.error("Error: variable 'a' must be 5");
+  //   console.expection("Error");
+
+  console.info("Information");
+
+  console.warn("Warning! Potential problems");
+
+  console.debug("For testing and fixing bugs");
+}
+//=========
+console.timeLog(counterLabel);
+
+console.count(counterLabel);
+//=========
+console.group("Group 1");
+
+console.log("Test");
+console.debug("Test 2");
+
+console.groupCollapsed("Group 2");
+
+console.warn("Test 3");
+
+console.groupEnd();
+
+console.groupEnd();
+
+//========
+
+console.timeEnd(counterLabel);
+
+console.countReset(counterLabel);
+
+console.count(counterLabel);
+
+*/
+
+//=======================================================================
+
+// const test1 = () => console.trace("Hello!");
+
+// const test2 = () => test1();
+// const test3 = () => test2();
+
+// test3();
+//=======================================================================
+
+// const data = [
+//   { name: "John", age: 25, city: "New York" },
+//   { name: "Alice", age: 30, city: "London" },
+// ];
+
+// // console.table(data);
+// console.table(data, ["name", "age"]);
+
+//===========================================================
+
+/* const a = 5;
+const b = 10;
+
+const result = a > b;
+
+console.assert(result, "Info");
+// if (a > b) {
+//   //....
+// } else {
+//   console.warn("Info");
+// }
+
+//====
+
+// console.clear(); // clear terminal
+
+//===========
+
+console.log(
+  "%cHello World",
+  "color: yellow; background-color: blue; font-size: 50px"
+);
+
+*/
+
+//=================================================================
