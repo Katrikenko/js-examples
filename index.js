5f3ce1a198c2ecaede9a7740bf8821f4125c6650
Kateryna Lysenko <katrikenko@gmail.com>
2023-07-28 15:30:59 +0200

    end func-constructor lesson

--- index.js
+++ index.js
@@ -1,188 +1,139 @@
-// const Animal = {
-//   name: "Animal",
-//   voice: "Sound",
-//   say() {
-//     console.log(`${this.name} says ${this.voice}`);
-//   },
+// const User = {
+//   login: null,
+//   password: null,
+//   role: null,
+//   age: null,
 // };
 
-// // const dog = { ...Animal };
-// const dog = Object.create(Animal);
+//======
+function User(data) {
+  //   console.log("new", new.target);
 
-// dog.name = "Milo";
-// dog.voice = "Bow wow";
+  if (new.target) {
+    const { login = null, password = null, isAdmin = null, age = 0 } = data;
 
-// dog.say();
+    const role = isAdmin ? "Admin" : "User";
 
-// console.log(dog);
+    const object = Object.assign(this, {
+      login,
+      password,
+      role,
+      age,
+    });
 
-// //========================
+    if (role === "Admin") {
+      object.verify = function (password) {
+        console.debug(password, this);
+        return this.password === password;
+      };
+    }
+    if (age >= 30) {
+      object.login = String(object.login).toUpperCase();
+    }
 
-// Object.defineProperty(dog, "_age", {
-//   value: 5,
-//   writable: true,
-// });
+    object.toString = function () {
+      return `User ${this.login}`;
+    };
 
-// // dog.age = 10;
+    // this.login = login;
+    // this.password = password;
+    // const role = isAdmin ? "Admin" : "User";
+    // this.age = age;
+    // const verify = function (password) {
+    //   return this.password === password;
+    // };
 
-// Object.defineProperty(dog, "age", {
-//   set(value) {
-//     this._age = value * 2;
-//   },
+    return object;
+  } else {
+    return new User(data); // if some constructor don't have 'new'
+  }
+}
 
-//   get() {
-//     return `${this._age || 0} years old`;
-//   },
+// function UserAdmin({ login = null, password = null, isAdmin = null, age = 0 }) {
+//   this.login = login;
+//   this.password = password;
+//   this.role = isAdmin ? "Admin" : "User";
+//   this.age = age;
 
-//   //   writable: true,
-//   //   configurable: true,
-//   //   enumerable: true,
-// });
+//   this.verify = function (password) {
+//     return this.password === password;
+//   };
+// }
+//=====
 
-// console.log(dog.age);
-
-// dog.age = 15;
-
-// console.log(dog.age);
-
-// delete dog._age;
-
-// console.log(Object.keys(dog));
-
-// //========================
-
-// Object.defineProperty(dog, "location", {
-//   value: "Ukraine",
-//   writable: true,
-//   configurable: true,
-//   enumerable: true,
-// });
-
-// console.log(dog.location);
-
-// console.log(Object.keys(dog));
-
-// dog.location = "Italy";
-
-// console.log(dog.location);
-
-// //========================
-
-// Object.defineProperty(dog, "_space", {
-//   value: 0,
-//   writable: true,
-// });
-
-// // dog.age = 10;
-
-// Object.defineProperty(dog, "space", {
-//   set(value) {
-//     this._space = value * 4;
-//   },
-
-//   get() {
-//     return `${this._space}px`;
-//   },
-// });
-
-// console.log(dog.space);
-
-// dog.space = 4;
-
-// console.log(Object.getOwnPropertyDescriptor(dog, "_space"));
-// console.log(dog.hasOwnProperty("voice"));
-
-// //=========================
-
-// Animal.go = function () {
-//   console.log(`${this.name} run`);
-// };
-
-// dog.go();
-
-// console.log(Object.getPrototypeOf(dog));
-
-// console.log(Animal.isPrototypeOf(dog));
-
-// Object.setPrototypeOf(dog, { animal: "big" });
-
-// console.log(Object.getPrototypeOf(dog));
-
-//=========================================================
-
-const Tag = {
-  render(value) {
-    return `<${this.className}>${value}<>`;
-  },
-  className: null,
+const registerData = {
+  login: "Ivan",
+  password: "51jk69d",
+  isAdmin: true,
+  age: 25,
 };
 
-// ===
+const user = User(registerData);
 
-const Button = Object.create(Tag);
+console.log(user.verify("tsgf"));
 
-Button.render = function (value = "") {
-  return `<button name="${this.className}" style="${this.style}">${value}</button>`;
+//================
+
+const adminData = {
+  login: "Hanna",
+  password: "345kjnFA7",
+  isAdmin: true,
 };
 
-// ===
+const admin = User(adminData);
+console.log(admin.password);
 
-const mainButtom = Object.create(Button, {
-  style: {
-    value: "background-color: blue;",
-    writable: true,
-  },
-  className: {
-    value: "my-button",
-  },
-});
+//=====
 
-console.log(mainButtom.render("Click"));
-console.log(mainButtom.className);
+User.prototype.test = "Hello, World!";
 
-// ===
+console.log(user.test);
+//======
 
-const Input = Object.create(Tag);
-
-Input.render = function () {
-  return `<input placeholder="${this.placeholder}" style="${this.style}">`;
+const testData = {
+  login: "Hanna",
+  password: "345kjnFA7",
+  age: 37,
 };
 
-const loginInput = Object.create(Input, {
-  style: {
-    value: "border: 1px, solid, green;",
-    writable: true,
-  },
-  placeholder: {
-    value: "Login...",
-  },
-});
+const testUser = new User(testData);
 
-console.log(loginInput.render());
-console.log(loginInput.className);
+console.log(testUser.login);
 
-// ===
+//=========
 
-const serverRequest = {
-  data: null,
-  getData: {
-    //......
-  },
-  render() {
-    this.data = this.getData();
+console.log(Object.getPrototypeOf(testUser) === User.prototype);
 
-    return `...`;
-  },
+console.log(user.toString());
+
+console.log(User.length);
+
+const test = User;
+
+console.log(test.name);
+
+//===========
+
+console.log("======================");
+
+console.log(user.verify("51jk69d"));
+
+// const verifyUser = user.verify;
+// console.log(verifyUser.apply(user, ["51jk69d"]));
+
+const verifyUser = user.verify.bind(user, "51jk69d");
+console.log(verifyUser());
+
+//============
+
+function Animal(name) {
+  this.name = name;
+}
+
+const Person = function (name, age) {
+  Animal.call(this, name);
+  this.age = age;
 };
 
-// ===
-
-const Page = {
-  components: [],
-  path: "/.../.../",
-  render() {
-    //......
-  },
-};
-
-// ===
+const user2 = new Person("Roman", 32);
+console.log(user2.name, user2.age);
--- index1.js
+++ index1.js
@@ -2524,3 +2524,195 @@ try {
 */
 
 //===========================================================================
+
+/* // const Animal = {
+//   name: "Animal",
+//   voice: "Sound",
+//   say() {
+//     console.log(`${this.name} says ${this.voice}`);
+//   },
+// };
+
+// // const dog = { ...Animal };
+// const dog = Object.create(Animal);
+
+// dog.name = "Milo";
+// dog.voice = "Bow wow";
+
+// dog.say();
+
+// console.log(dog);
+
+// //========================
+
+// Object.defineProperty(dog, "_age", {
+//   value: 5,
+//   writable: true,
+// });
+
+// // dog.age = 10;
+
+// Object.defineProperty(dog, "age", {
+//   set(value) {
+//     this._age = value * 2;
+//   },
+
+//   get() {
+//     return `${this._age || 0} years old`;
+//   },
+
+//   //   writable: true,
+//   //   configurable: true,
+//   //   enumerable: true,
+// });
+
+// console.log(dog.age);
+
+// dog.age = 15;
+
+// console.log(dog.age);
+
+// delete dog._age;
+
+// console.log(Object.keys(dog));
+
+// //========================
+
+// Object.defineProperty(dog, "location", {
+//   value: "Ukraine",
+//   writable: true,
+//   configurable: true,
+//   enumerable: true,
+// });
+
+// console.log(dog.location);
+
+// console.log(Object.keys(dog));
+
+// dog.location = "Italy";
+
+// console.log(dog.location);
+
+// //========================
+
+// Object.defineProperty(dog, "_space", {
+//   value: 0,
+//   writable: true,
+// });
+
+// // dog.age = 10;
+
+// Object.defineProperty(dog, "space", {
+//   set(value) {
+//     this._space = value * 4;
+//   },
+
+//   get() {
+//     return `${this._space}px`;
+//   },
+// });
+
+// console.log(dog.space);
+
+// dog.space = 4;
+
+// console.log(Object.getOwnPropertyDescriptor(dog, "_space"));
+// console.log(dog.hasOwnProperty("voice"));
+
+// //=========================
+
+// Animal.go = function () {
+//   console.log(`${this.name} run`);
+// };
+
+// dog.go();
+
+// console.log(Object.getPrototypeOf(dog));
+
+// console.log(Animal.isPrototypeOf(dog));
+
+// Object.setPrototypeOf(dog, { animal: "big" });
+
+// console.log(Object.getPrototypeOf(dog));
+
+//=========================================================
+
+const Tag = {
+  render(value) {
+    return `<${this.className}>${value}<>`;
+  },
+  className: null,
+};
+
+// ===
+
+const Button = Object.create(Tag);
+
+Button.render = function (value = "") {
+  return `<button name="${this.className}" style="${this.style}">${value}</button>`;
+};
+
+// ===
+
+const mainButtom = Object.create(Button, {
+  style: {
+    value: "background-color: blue;",
+    writable: true,
+  },
+  className: {
+    value: "my-button",
+  },
+});
+
+console.log(mainButtom.render("Click"));
+console.log(mainButtom.className);
+
+// ===
+
+const Input = Object.create(Tag);
+
+Input.render = function () {
+  return `<input placeholder="${this.placeholder}" style="${this.style}">`;
+};
+
+const loginInput = Object.create(Input, {
+  style: {
+    value: "border: 1px, solid, green;",
+    writable: true,
+  },
+  placeholder: {
+    value: "Login...",
+  },
+});
+
+console.log(loginInput.render());
+console.log(loginInput.className);
+
+// ===
+
+const serverRequest = {
+  data: null,
+  getData: {
+    //......
+  },
+  render() {
+    this.data = this.getData();
+
+    return `...`;
+  },
+};
+
+// ===
+
+const Page = {
+  components: [],
+  path: "/.../.../",
+  render() {
+    //......
+  },
+};
+
+// ===
+*/
+
+//====================================================================
