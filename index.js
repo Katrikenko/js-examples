491c8a1dc6b7dcd780626f8a26069e669b94e2a2
Kateryna Lysenko <katrikenko@gmail.com>
2023-08-03 22:18:45 +0200

    patterns p.1

--- index.js
+++ index.js
@@ -1,86 +1,458 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
+/*
+// // class RecentPurchases {
+// //   static #instance = null;
 
-  test = () => {
-    console.log("Hello, World!", this.name);
-  };
+// //   constructor() {
+// //     this.purchases = [];
+// //   }
+
+// //   static create() {
+// //     if (!this.#instance) {
+// //       this.#instance = new RecentPurchases();
+// //     }
+
+// //     return this.#instance;
+// //   }
+
+// //   add(item) {
+// //     this.purchases.push(item);
+// //   }
+
+// //   get() {
+// //     return this.purchases;
+// //   }
+// // }
+
+// // const lastPurchaseList = RecentPurchases.create();
+// // //=======
+// // const lastPurchaseList2 = RecentPurchases.create();
+
+// // console.log(lastPurchaseList === lastPurchaseList2);
+
+// // lastPurchaseList2.add("Phone");
+// // lastPurchaseList2.add("Earphones");
+
+// // console.log(lastPurchaseList.get());
+
+// // =========================================================
+
+// class RecentPurchases {
+//   static #instance = null;
+
+//   static #purchases = [];
+
+//   static create() {
+//     if (!this.#instance) {
+//       this.#instance = new RecentPurchases();
+//     }
+
+//     return this.#instance;
+//   }
+
+//   static add(item) {
+//     this.#purchases.push(item);
+//   }
+
+//   static get() {
+//     return this.#purchases;
+//   }
+// }
+
+// RecentPurchases.create();
+
+// RecentPurchases.add("Phone");
+// RecentPurchases.add("Earphones");
+// //=======
+
+// console.log(RecentPurchases.get());
+
+// // =======================================================
+// class Button {
+//   constructor({ text, color }) {
+//     this.text = text;
+//     this.color = color;
+//   }
+
+//   render() {
+//     return `<button class="color:${this.color};">${this.text}</button>`;
+//   }
+// }
+
+// class IconButton {
+//   constructor({ icon, color }) {
+//     this.icon = icon;
+//     this.color = color;
+//   }
+
+//   render() {
+//     return `<button class="color:${this.color};"><img src="${this.icon}"></button>`;
+//   }
+// }
+
+// class ButtonFactory {
+//   static TYPE = {
+//     BASIC: "basic",
+//     ICON: "icon",
+//   };
+
+//   static createButton(type, options) {
+//     if (options.icon) {
+//       return new IconButton(options);
+//     }
+//     if (options.text) {
+//       return new Button(options);
+//     }
+
+//     throw new Error(`This type of button doesn't exist: ${type}`);
+
+//     // switch (type) {
+//     //   case this.TYPE.BASIC:
+//     //     return new Button(options);
+//     //   case this.TYPE.ICON:
+//     //     return new IconButton(options);
+//     //   default:
+//     //     throw new Error(`This type of button doesn't exist: ${type}`);
+//     // }
+//   }
+// }
+
+// const myIconButton = ButtonFactory.createButton(ButtonFactory.TYPE.ICON, {
+//   color: "red",
+//   icon: "/icon/my-icon.svg",
+// });
+
+// console.log(myIconButton);
+
+// //===================================================================================
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   sendEmail(message) {
+//     console.log(`Sending message: ${message} to e-mail ${this.email}`);
+//   }
+// }
+
+// class Video {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// class Channel {
+//   constructor(name) {
+//     this.name = name;
+//     this.subscribers = [];
+//   }
+
+//   subscribe(user) {
+//     this.subscribers.push(user);
+//   }
+
+//   unsubscribe(user) {
+//     this.subscribers = this.subscribers.filter((sub) => sub !== user);
+//   }
+
+//   createVideo(name) {
+//     const video = new Video(name);
+//     this.sendNotify(video);
+//   }
+
+//   sendNotify(video) {
+//     this.subscribers.forEach((subscriber) => {
+//       subscriber.sendEmail(
+//         `New video "${video.name}" on Youtube chanel ${this.name}!`
+//       );
+//     });
+//   }
+// }
+
+// const channel = new Channel("IT Brains");
+
+// const user1 = new User("john@example.com");
+// const user2 = new User("jane@example.com");
+// const user3 = new User("alice@example.com");
+
+// channel.subscribe(user1);
+// channel.subscribe(user2);
+// channel.subscribe(user3);
+
+// channel.createVideo("HTML Lesson");
+
+// console.log("=======");
+
+// channel.unsubscribe(user1);
+
+// channel.createVideo("CSS Lesson");
+
+// //===================================
+
+class Coffee {
+  name = "Coffee";
+
+  cost = 10;
+
+  cook() {
+    console.log(`Preparing ${this.name}`);
+  }
 }
 
-class User extends Person {
-  constructor(login, password) {
-    super(login);
-
-    this.login = login;
-    this.password = password;
+class MilkDecorator {
+  constructor(coffee, amount) {
+    this.coffee = coffee;
+    this.amount = amount;
   }
 
-  login = null;
-  password = null;
-  #role = null;
-  //   static age = null;
-  age = null;
+  get name() {
+    return `${this.coffee.name} with ${this.amount} ml of milk`;
+  }
 
-  id = null;
-  #id = 1000;
+  get cost() {
+    const milkPrice = 0.05;
+    return this.coffee.cost + milkPrice * this.amount;
+  }
 
-  isAdmin = () => {
-    console.log(this.#id);
-    return this.role === "Admin";
-  };
+  cook() {
+    console.log(`Preparing ${this.name}`);
+  }
+}
 
-  createAdminUser = (login) => {
-    const password = this.generateRandomPassword();
-    return new User();
-  };
+let coffee = new Coffee();
+console.log(coffee.name);
+console.log(coffee.cost);
+coffee.cook();
 
-  static generateRandomPassword = () => {
+let latteCoffee = new MilkDecorator(coffee, 300);
+console.log(latteCoffee.name);
+console.log(latteCoffee.cost);
+latteCoffee.cook();
+
+//=======================================================
+
+console.log("//=======================================================");
+
+class TextEditor {
+  #text = "";
+
+  set text(text) {
+    this.#text = text;
+    this.#save();
+  }
+
+  get text() {
+    return this.#text;
+  }
+
+  #save() {
+    Snapshot.create(this.text);
+  }
+
+  restore() {
+    this.#text = Snapshot.restore().text;
+  }
+}
+
+class Snapshot {
+  constructor(text) {
+    this.text = text;
+  }
+
+  static #snapshots = [];
+
+  static create(text) {
+    console.log(text);
+    this.#snapshots.push(new Snapshot(text));
+  }
+
+  static restore() {
+    this.#snapshots.pop();
+    return this.#snapshots[this.#snapshots.length - 1];
+  }
+}
+
+const editor = new TextEditor();
+
+editor.text = "It is initial text.";
+editor.text = "It is edited text.";
+editor.text = "It is updated text.";
+
+console.log("=========");
+
+console.log(editor.text);
+
+console.log("=========");
+
+editor.restore();
+
+console.log(editor.text);
+
+editor.restore();
+
+console.log(editor.text);
+
+//=======================================================
+
+console.log("//=======================================================");
+
+class AuthHandler {
+  setNextHandler(handler) {
+    this.nextHandler = handler;
+    return handler;
+  }
+
+  login(user, password) {
+    if (this.nextHandler) {
+      return this.nextHandler.login(user, password);
+    } else {
+      return false;
+    }
+  }
+}
+
+class TwoFactorAuthHandler extends AuthHandler {
+  login(user, password) {
+    if (
+      user === "john" &&
+      password === "password" &&
+      this.isValidTwoFactorCode()
+    ) {
+      console.log("Login is allowed with two-factor authentication");
+      return true;
+    } else {
+      return super.login(user, password);
+    }
+  }
+
+  isValidTwoFactorCode() {
     return true;
-  };
-
-  get admin() {
-    return this.#role === "Admin";
-  }
-
-  set admin(value) {
-    this.#role = "Admin";
   }
 }
 
-const user = new User("Gennady", "27cMx5q38");
+//==========
 
-// console.log(User.prototype === user.__proto__);
-// console.log(user);
-
-// console.log(user.isAdmin());
-
-function verifyAdmin(fn) {
-  const result = fn();
-
-  if (!result) {
-    throw new Error("Not an Admin");
+class RoleHander extends AuthHandler {
+  login(user, password) {
+    if (user === "guest") {
+      console.log("Login is allowed with the guest role");
+      return true;
+    } else {
+      return super.login(user, password);
+    }
   }
-
-  return true;
 }
 
-// verifyAdmin(user.isAdmin);
+class CretentialsHandler extends AuthHandler {
+  login(user, password) {
+    if (user === "admin" && password === "admin123") {
+      console.log("Login is permitted by login and password");
+      return true;
+    } else {
+      return super.login(user, password);
+    }
+  }
+}
 
-// console.log(user);
-// console.log(User.generateRandomPassword());
-// console.log(User);
+//==================================================================
 
-//=======
+class HandlerBuilder {
+  constructor() {
+    this.firstHandler = null;
+    this.lastHandler = null;
+  }
 
-// console.log(user.admin);
+  add(handler) {
+    if (!this.firstHandler) {
+      this.firstHandler = handler;
+      this.lastHandler = handler;
+    } else {
+      this.lastHandler.setNextHandler(handler);
+      this.lastHandler = handler;
+    }
+    return this;
+  }
 
-// user.admin = true;
+  create() {
+    return this.firstHandler;
+  }
+}
 
-// console.log(user.admin);
+//=========
 
-//======
+// const handler = new TwoFactorAuthHandler();
 
-// console.log(user.test());
+// handler.setNextHandler({
+//   login: (login, password) => {
+//     const result =
+//       login === "login" && password === "password"
+//         ? "The user is logged into the account"
+//         : "The user does not logged into the account";
 
-console.log(user instanceof User);
+//     console.log(result);
+//     return result;
+//   },
+// });
+// handler.setNextHandler(new CretentialsHandler());
+
+// // handler.login("login", "password");
+// handler.login("admin", "admin123");
+
+// const handler2 = new CretentialsHandler();
+
+// handler2.setNextHandler(new RoleHander());
+
+// handler.setNextHandler(handler2);
+
+// handler.login("guest", "admin123");
+
+const handlerBuilder = new HandlerBuilder();
+
+const handler = handlerBuilder
+  .add(new CretentialsHandler())
+  .add(new TwoFactorAuthHandler())
+  .add(new RoleHander())
+  .create();
+
+handler.login("admin", "admin123");
+handler.login("john", "password");
+handler.login("guest", "guest123");
+handler.login("user", "password");
+*/
+
+//============================================================
+
+class User {
+  constructor(name, messenger) {
+    this.name = name;
+    this.messenger = messenger;
+  }
+
+  sendMessage(message) {
+    const formattedMessage = this.formatMessage(message);
+    // return formattedMessage;
+    this.messenger.sendMessage(formattedMessage);
+  }
+
+  formatMessage(message) {
+    return `[${this.name}]: ${message}`;
+  }
+}
+
+class SMSMessenger {
+  static sendMessage(message) {
+    console.log(`SMS sent: ${message}`);
+  }
+}
+
+class EmailMessenger {
+  static sendMessage(message) {
+    console.log(`Email sent: ${message}`);
+  }
+}
+
+const john = new User("John", SMSMessenger);
+const jane = new User("Jane", EmailMessenger);
+
+john.sendMessage("Hi!");
+jane.sendMessage("Hello!");
--- index1.js
+++ index1.js
@@ -2859,3 +2859,93 @@ console.log(user2.name, user2.age);
 */
 
 //============================================================================
+
+/* class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  test = () => {
+    console.log("Hello, World!", this.name);
+  };
+}
+
+class User extends Person {
+  constructor(login, password) {
+    super(login);
+
+    this.login = login;
+    this.password = password;
+  }
+
+  login = null;
+  password = null;
+  #role = null;
+  //   static age = null;
+  age = null;
+
+  id = null;
+  #id = 1000;
+
+  isAdmin = () => {
+    console.log(this.#id);
+    return this.role === "Admin";
+  };
+
+  createAdminUser = (login) => {
+    const password = this.generateRandomPassword();
+    return new User();
+  };
+
+  static generateRandomPassword = () => {
+    return true;
+  };
+
+  get admin() {
+    return this.#role === "Admin";
+  }
+
+  set admin(value) {
+    this.#role = "Admin";
+  }
+}
+
+const user = new User("Gennady", "27cMx5q38");
+
+// console.log(User.prototype === user.__proto__);
+// console.log(user);
+
+// console.log(user.isAdmin());
+
+function verifyAdmin(fn) {
+  const result = fn();
+
+  if (!result) {
+    throw new Error("Not an Admin");
+  }
+
+  return true;
+}
+
+// verifyAdmin(user.isAdmin);
+
+// console.log(user);
+// console.log(User.generateRandomPassword());
+// console.log(User);
+
+//=======
+
+// console.log(user.admin);
+
+// user.admin = true;
+
+// console.log(user.admin);
+
+//======
+
+// console.log(user.test());
+
+console.log(user instanceof User);
+*/
+
+//======================================================================
